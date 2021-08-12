94b063be866dc039ab55c7a686322c5a3394375e
Carlos Poveda <c.poveda@outlook.com>
2021-08-12 04:48:42 -0600

    next upgrade v11.1.0

--- components/card/card.js
+++ components/card/card.js
@@ -8,7 +8,7 @@ const Card = (props) => {
     return (
         <article className={styles.CardStyle}>  
             <div className={styles.ImageWrapper}>
-            <Image src={props.fotoURL ?? "https://i.stack.imgur.com/y9DpT.jpg"} layout='fill' alt={props.children}  placeholder="blur"/>
+            <Image src={props.fotoURL ?? "https://i.stack.imgur.com/y9DpT.jpg"} layout='fill' alt={props.children}/>
             </div>
             <p className={styles.date}>{props.date.slice(0,10)}</p>
               <Link href={props.url}>
--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import styles from './footer.module.css'
 
 const footer = () => {
@@ -5,6 +6,9 @@ const footer = () => {
     const year = date.getFullYear()
     return (
         <footer className={styles.footerContainer}>
+
+        <div id="wcb" className="carbonbadge"></div>
+        <Script src="https://unpkg.com/website-carbon-badges@1.1.3/b.min.js" defer/>
 
             &copy; {year} Paisaje Centinela
         </footer>
