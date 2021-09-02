d5a23bf63c4421ec86bb675e83a3025efd9c562d
Carlos Poveda <c.poveda@outlook.com>
2021-09-02 11:39:54 -0600

    allow-access-control-origin header

--- next.config.js
+++ next.config.js
@@ -2,5 +2,19 @@ module.exports = {
   images: {
     domains: ['images.ctfassets.net','i.stack.imgur.com','genius100visions.com'],
   },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin:',
+            value: 'https://paisajecentinela.org',
+          }
+        ],
+      },
+    ]
+  },
   
 }
